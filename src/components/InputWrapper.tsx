96b08fd63858e25d35167b42d62159fb70836d4e
Maik Emanoel <maikemanoel11@gmail.com>
2023-09-02 16:21:50 -0300

    pass more props

--- src/components/InputWrapper.tsx
+++ src/components/InputWrapper.tsx
@@ -1,16 +1,28 @@
 interface InputWrapperProps {
-    id: string,
-    label: string
+  id: string;
+  label: string;
+  goal: number;
+  max: number;
+  step: number;
+  setFunction: React.Dispatch<React.SetStateAction<number>>
+  value: number
 }
 
 export function InputWrapper(prop: InputWrapperProps) {
-    return (
-        <div>
-            <div>
-                <label htmlFor={prop.id}>{prop.label}</label>
-                <span>10x</span>
-            </div>
-            <input type="range" id={prop.id} />
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <label htmlFor={prop.id}>{prop.label}</label>
+        <span>{prop.goal}x</span>
+      </div>
+      <input
+        type="range"
+        id={prop.id}
+        max={prop.max}
+        step={prop.step}
+        onChange={(e) => prop.setFunction(Number(e.target.value))}
+        value={prop.value}
+      />
+    </div>
+  );
 }
