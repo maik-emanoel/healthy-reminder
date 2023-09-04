2ce17064382edc3f0c1ccc5f672263b355903d26
Maik Emanoel <maikemanoel11@gmail.com>
2023-09-04 10:54:53 -0300

    style InputWrapper

--- src/components/InputWrapper.tsx
+++ src/components/InputWrapper.tsx
@@ -4,21 +4,27 @@ interface InputWrapperProps {
   goal: number;
   max: number;
   step: number;
-  setFunction: React.Dispatch<React.SetStateAction<number>>
-  value: number
+  setFunction: React.Dispatch<React.SetStateAction<number>>;
+  value: number;
 }
 
 export function InputWrapper(prop: InputWrapperProps) {
   return (
-    <div>
-      <div>
-        <label htmlFor={prop.id}>{prop.label}</label>
-        <span>{prop.goal}x</span>
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-between items-center font-medium">
+        <label
+          htmlFor={prop.id}
+          className="leading-4 tracking-[0.48px] text-blue"
+        >
+          {prop.label}
+        </label>
+        <span className="text-xs tracking-[0.36px] text-[#4E4964]">{prop.goal}x</span>
       </div>
       <input
         type="range"
         id={prop.id}
         max={prop.max}
+        min={5}
         step={prop.step}
         onChange={(e) => prop.setFunction(Number(e.target.value))}
         value={prop.value}
