7d1b27ca75294be52aea2ac41bbb4267161c4d67
Maik Emanoel <maikemanoel11@gmail.com>
2023-09-04 10:56:52 -0300

    fix timer function & style elements

--- src/App.tsx
+++ src/App.tsx
@@ -9,64 +9,77 @@ import sticker from "./assets/sticker.png";
 export function App() {
   const [dailyGoal, setDailyGoal] = useState<number>(50);
   const [quantity, setQuantity] = useState<number>(5);
-  const [partialPercentage, setPartialPercentage] = useState(0)
+  const [partialPercentage, setPartialPercentage] = useState(0);
 
-  const [hours, setHours] = useState(0)
-  const [minutes, setMinutes] = useState(0)
-  
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+
   function turnIntoSeconds() {
-    const hoursToSeconds = hours * 3600
-    const minutesToSeconds = minutes * 60
+    const hoursToSeconds = hours * 3600;
+    const minutesToSeconds = minutes * 60;
 
-    console.log(`Hora para segundos: ${hoursToSeconds}, Minutos para segundos: ${minutesToSeconds}`)
-    return { hoursToSeconds, minutesToSeconds }
+    console.log(
+      `Hora para segundos: ${hoursToSeconds}, Minutos para segundos: ${minutesToSeconds}`
+    );
+    return { hoursToSeconds, minutesToSeconds };
   }
 
   function timer() {
-    const { hoursToSeconds, minutesToSeconds } = turnIntoSeconds()
-    let totalSeconds = hoursToSeconds + minutesToSeconds
+    const { hoursToSeconds, minutesToSeconds } = turnIntoSeconds();
+    let totalSeconds = hoursToSeconds + minutesToSeconds;
 
     const decreaseTime = setInterval(() => {
-      totalSeconds--
-      console.log(totalSeconds)
+      totalSeconds--;
+      console.log(totalSeconds);
 
-      if(totalSeconds === 0) {
-        clearInterval(decreaseTime)
-        const calcPercentage = (quantity / dailyGoal) * 100
-        setPartialPercentage(prevPercentage => prevPercentage + calcPercentage)
-        
-        if(partialPercentage >=  100) {
-          setPartialPercentage(100)
+      if (totalSeconds === 0) {
+        clearInterval(decreaseTime);
+        const calcPercentage = (quantity / dailyGoal) * 100;
+        const roundedPercentage = Math.round(calcPercentage)
+        setPartialPercentage(
+          (prevPercentage) => prevPercentage + roundedPercentage
+        );
+
+        if (partialPercentage >= 100) {
+          setPartialPercentage(100);
         }
       }
-    }, 100) 
+    }, 100);
   }
 
   return (
     <div className="w-full h-screen bg-gray-500 text-gray-100">
       <main className="max-w-[690px] w-[90%] mx-auto pt-44">
-        <header>
-          <div>
+        <header className="flex items-center gap-2 mb-[76px]">
+          <div className="w-16 h-16 rounded-full bg-gray-200 grid place-items-center">
             <img src={sticker} alt="Avatar" />
           </div>
-          <div>
-            <span>Boa tarde,</span>
-            <span>Biro 👋</span>
+          <div className="flex flex-col gap-1 text-2xl leading-7">
+            <span className="font-light">Boa tarde,</span>
+            <span className="font-semibold">Biro 👋</span>
           </div>
         </header>
 
-        <div>
-          <div>
-            <span>{partialPercentage}%</span>
-            <img src={person} alt="Pessoa dando uma cambalhota" />
+        <div className="flex justify-between">
+          <div className="bg-blue text-[#1F2128] px-10 py-8 rounded-[20px] text-center w-52 h-[340px] flex flex-col">
+            <span className="text-xs font-bold tracking-[0.36px] mb-10">
+              {partialPercentage}%
+            </span>
+            <div className="flex-1">
+              <img
+                src={person}
+                alt="Pessoa dando uma cambalhota"
+                className="mx-auto"
+              />
+            </div>
 
-            <strong>Alongar</strong>
-            <p>
+            <strong className="text-xl">Alongar</strong>
+            <p className="font-medium tracking-[0.48px]">
               Meta: <span>{dailyGoal}</span>x
             </p>
           </div>
 
-          <div>
+          <div className="max-w-[320px] w-full flex flex-col justify-between">
             <InputWrapper
               id="inputDailyGoal"
               label="Meta diária"
@@ -86,37 +99,44 @@ export function App() {
               value={quantity}
             />
 
-            <div className="text-black">
-              <div>
+            <div className="text-white flex items-center gap-3 mx-auto">
+              <div className="timeInputWrapper">
                 <IMaskInput
                   mask="00"
                   validate={(value) => {
                     return Number(value) <= 23;
                   }}
                   onAccept={(e) => {
-                    setHours(Number(e))
+                    setHours(Number(e));
                   }}
+                  className="timeInput"
+                  placeholder="00"
                 />
-                <span>h</span>
+                <span className="text-gray-200">h</span>
               </div>
-              <span>:</span>
-              <div>
+              <span className="text-[32px]">:</span>
+              <div className="timeInputWrapper">
                 <IMaskInput
                   mask="00"
                   validate={(value) => {
                     return Number(value) <= 59;
                   }}
                   onAccept={(e) => {
-                    setMinutes(Number(e))
+                    setMinutes(Number(e));
                   }}
+                  className="timeInput"
+                  placeholder="00"
                 />
-                <span>m</span>
+                <span className="text-gray-200">m</span>
               </div>
             </div>
-            <button onClick={() => {
-              timer()
-            }}>
-              <span>Começar</span>
+            <button
+              onClick={() => {
+                timer();
+              }}
+              className="bg-blue h-14 rounded-md text-gray-500 flex items-center justify-center gap-2"
+            >
+              <span className="font-medium tracking-[0.48px]">Começar</span>
               <ChevronRight />
             </button>
           </div>
