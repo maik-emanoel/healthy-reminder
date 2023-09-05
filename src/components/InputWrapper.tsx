1f4a3cbc1724c75ca609b572ab7fe60dbadf75de
Maik Emanoel <maikemanoel11@gmail.com>
2023-09-05 09:21:44 -0300

    feat: create function handleInputColor

--- src/components/InputWrapper.tsx
+++ src/components/InputWrapper.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 interface InputWrapperProps {
   id: string;
   label: string;
@@ -9,26 +11,50 @@ interface InputWrapperProps {
 }
 
 export function InputWrapper(prop: InputWrapperProps) {
+  const inputRangeRef = useRef<HTMLInputElement | null>(null)
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleInputColor(e: any) {
+    const progress = (Number(e.target.value) / Number(e.target.max)) * 100 
+
+    if(inputRangeRef.current) {
+      inputRangeRef.current.style.background = `linear-gradient(to right, #7FC4ED ${progress}%, #322F40 ${progress}%)`
+    }
+  }
+
+  useEffect(() => {
+    if (inputRangeRef.current) {
+      handleInputColor({ target: inputRangeRef.current });
+    }
+  }, []); 
+
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-3">
       <div className="flex justify-between items-center font-medium">
         <label
           htmlFor={prop.id}
-          className="leading-4 tracking-[0.48px] text-blue"
+          className="leading-4 tracking-[0.48px] text-blue mb-1"
         >
           {prop.label}
         </label>
-        <span className="text-xs tracking-[0.36px] text-[#4E4964]">{prop.goal}x</span>
+        <span className="text-xs tracking-[0.36px] text-[#4E4964]">
+          {prop.goal}x
+        </span>
       </div>
       <input
         type="range"
         id={prop.id}
         max={prop.max}
-        min={5}
         step={prop.step}
-        onChange={(e) => prop.setFunction(Number(e.target.value))}
+        onChange={(e) => {
+          prop.setFunction(Number(e.target.value));
+          handleInputColor(e)
+        }}
         value={prop.value}
+        ref={inputRangeRef}
       />
     </div>
   );
 }
+
+
