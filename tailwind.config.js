f060e3ee48e088212308e20b33944863759de4f7
Maik Emanoel <maikemanoel11@gmail.com>
2023-09-06 14:59:16 -0300

    add max-screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,23 @@ export default {
             transform: 'translateY(-10px)'
           }
         }
-      }
+      },
+      screens: {
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '700px'},
+        // => @media (max-width: 700px) { ... }
+  
+        'sm': {'max': '425px'},
+        // => @media (max-width: 425px) { ... }
+      },
     },
   },
   plugins: [],
